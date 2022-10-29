d60c447703d239e5331ac265e9320c84e3604f26
TosiaDan <antonina.ivanets@gmail.com>
2022-10-29 23:22:54 +0200

    new js

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,10 @@
+export default function fetchCountries() {
+  const searchParams = 'name,capital,population,flags,languages';
+  const url = `https://restcountries.com/v3.1/all?fields=${searchParams}`;
+  return fetch(url).then(r => {
+    if (!r.ok) {
+      throw new Error(r.status);
+    }
+    return r.json();
+  });
+}
